9ef40b98521f34c9d98fbba08bc1412bf8168ad8
Yang <yang.fu@di-on.solutions>
2023-07-09 21:31:40 +0200

    remove js from gitignore

---
+++ my-app/out/_next/static/f1d1BhmnQCipRZUOyr-Ld/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ef46db3751d8e999.css","static/chunks/pages/index-c99f8dc618ea0608.js"],"/_error":["static/chunks/pages/_error-7fafba9435aeb6bc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
